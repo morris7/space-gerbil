fb2ed117b4446dc120051936aef299b7d5e3cbb3
Sam Morris <samuel.alan.morris@gmail.com>
2015-12-17 22:47:54 +0700

    updated gerbil motion

--- js/spaceGerbil.js
+++ js/spaceGerbil.js
@@ -118,29 +118,37 @@ function update() {
     if (cursors.left.isDown)
     {
         //  Move to the left
-        player.body.velocity.x = -200;
-        player.body.velocity.y = 0;
+        if(player.body.velocity.x > -150) {
+            player.body.velocity.x -= 20;
+        }
+        //player.body.velocity.y = 0;
 
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = 200;
-        player.body.velocity.y = 0;
+        if(player.body.velocity.x < 150) {
+            player.body.velocity.x += 20;
+        }
+        //player.body.velocity.y = 0;
 
         player.animations.play('right');
     }
     else if (cursors.up.isDown)
     {
-        player.body.velocity.y = -200;
-        player.body.velocity.x = 0;
+        if(player.body.velocity.y > -150) {
+            player.body.velocity.y -= 20;
+        }
+        //player.body.velocity.x = 0;
         player.animations.play('up');
     }
     else if (cursors.down.isDown)
     {
-        player.body.velocity.y = 200;
-        player.body.velocity.x = 0;
+        if(player.body.velocity.y < 150) {
+            player.body.velocity.y += 20;
+        }
+        //player.body.velocity.x = 0;
         player.animations.play('down');
     }
     else
